c57c0c505f081a6b50ffaeacc91808ce402cb527
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-09-18 14:49:00 +0900

    세팅 중 (3)

--- MatZipSpring/src/main/java/com/koreait/matzip/user/UserController.java
+++ MatZipSpring/src/main/java/com/koreait/matzip/user/UserController.java
@@ -1,5 +1,6 @@
 package com.koreait.matzip.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.koreait.matzip.Const;
 import com.koreait.matzip.ViewRef;
+import com.koreait.matzip.user.model.UserVO;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	
+	@Autowired
+	private UserService service;
 	
 	@RequestMapping(value="/login", method = RequestMethod.GET)
 	public String login(Model model) {
@@ -18,4 +23,19 @@ public class UserController {
 		model.addAttribute(Const.VIEW, "user/login");
 		return ViewRef.TEMP_DEFAULT;
 	}
+	
+	@RequestMapping(value="/login", method = RequestMethod.POST) 
+	public String login(UserVO param) {
+		int result = service.login(param);
+		
+		if(result == 1) {
+			return "redirect:/rest/map";
+		}
+		return "redirect:/user/login?err=" + result;
+	}
 }
+
+
+
+
+
--- MatZipSpring/src/main/webapp/WEB-INF/views/user/login.jsp
+++ MatZipSpring/src/main/webapp/WEB-INF/views/user/login.jsp
@@ -3,7 +3,7 @@
 <div id="sectionContainerCenter">
 	<div>
 		<div class="msg">${msg}</div>
-		<form class="frm" action="/user/loginProc" method="post">
+		<form class="frm" action="/user/login" method="post">
 			<div><input type="text" name="user_id" placeholder="아이디"></div>
 			<div><input type="password" name="user_pw" placeholder="비밀번호"></div>
 			<div><input type="submit" value="로그인"></div>
