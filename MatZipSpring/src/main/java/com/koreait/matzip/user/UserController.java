c0013a8bfdc7e8fcad09cb93b829143cf9c66d64
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-09-21 10:21:16 +0900

    로그인 작업 중

--- MatZipSpring/src/main/java/com/koreait/matzip/user/UserController.java
+++ MatZipSpring/src/main/java/com/koreait/matzip/user/UserController.java
@@ -1,15 +1,18 @@
 package com.koreait.matzip.user;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.koreait.matzip.Const;
 import com.koreait.matzip.ViewRef;
-import com.koreait.matzip.user.model.UserDTO;
+import com.koreait.matzip.user.model.UserPARAM;
 import com.koreait.matzip.user.model.UserVO;
 
 @Controller
@@ -27,13 +30,15 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/login", method = RequestMethod.POST) 
-	public String login(UserDTO param) {
+	public String login(UserPARAM param, HttpSession hs, RedirectAttributes ra) {
 		int result = service.login(param);
 		
-		if(result == 1) {
+		if(result == Const.SUCCESS) {
+			hs.setAttribute(Const.LOGIN_USER, param);
 			return "redirect:/rest/map";
 		}
-		return "redirect:/user/login?err=" + result;
+		ra.addAttribute("param", param);
+		return "redirect:/user/login";
 	}
 	
 	@RequestMapping(value="/join", method = RequestMethod.GET)
